eedc30d41ac2e5fb9bda6116b1feda79587ddda4
AngelTM <angel1997amg@gmail.com>
2023-07-31 15:12:22 -0700

    deleted token

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,5 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:"skC4rCAdXPqEne3WL538dac3TaFwmFJGqbq6wbpJUcSJvpaTMDHQQM8QxJv2lcKvnaj91Li9qO6Obky0W9uC3S2pMedaf31SYXszAWCJuskNTCCEg4pGGrPabU8aBRo2O1EfBPbX5QlT6ibJBUct2rNrSXxm4K3gGHRrftTs9cBJ8TtdlcTS"
+  token:""
 })
