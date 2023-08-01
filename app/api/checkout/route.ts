7c9a2b0784d2395dcf02f3c00720e87638106cfd
AngelTM <angel1997amg@gmail.com>
2023-08-01 15:20:49 -0700

    add shopping cart and stripe

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -3,6 +3,30 @@ import { NextResponse } from "next/server"
 import { validateCartItems } from "use-shopping-cart/utilities"
 
 import { inventory } from "@/config/inventory"
-import { stripe } from "@/lib/stripe"
+import { stripe,stripePromise } from "@/lib/stripe"
 
-export async function POST() {}
+export async function POST(request:Request) {
+    const cartDetails = await request.json()
+    const lineItems = validateCartItems(inventory,cartDetails)
+    const origin= request.headers.get("origin")
+    //const loadStripe = await stripePromise
+    const session = await stripe.checkout.sessions.create({
+        submit_type:"pay",
+        mode:"payment",
+        payment_method_types:['card'],
+        line_items: lineItems,
+        shipping_address_collection:{
+            allowed_countries:['US']
+        },
+        shipping_options:[
+            {
+                shipping_rate:"shr_1NaOtQGB8qivC6H0isqVnpSA"
+            }
+        ],
+        billing_address_collection: "auto",
+        success_url:`${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url:`${origin}/cart`
+    })
+    console.log("RE")
+    return NextResponse.json(session)
+}
--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,10 @@
+import { loadStripe } from "@stripe/stripe-js";
 import Stripe from "stripe"
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2022-11-15",
 })
+
+export const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
